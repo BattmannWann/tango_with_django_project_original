662ea732562914954c70f3f6913bd5ecacd8879e
Rhys Stewart <2682261s@student.gla.ac.uk>
2024-01-16 19:49:10 +0000

    Completed chapter 4.1 about templates, website works as expected

--- rango/views.py
+++ rango/views.py
@@ -4,13 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     
-    response = """ Rango says hey there partner! 
+    #boldmessage matches to the {{ boldmessage }} in the template
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     
-                   <a href = '/rango/about/'>About</a>
-            
-            """
-
-    return HttpResponse(response)
+    #note that the first parameter below is the TEMPLATE WE WISH TO USE
+    return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
     
