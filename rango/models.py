22c38eae7faa28184610f359b55b551be642bd98
Rhys Stewart <2682261s@student.gla.ac.uk>
2024-01-24 13:11:58 +0000

    Completed chapter 5.3, added classes to models.py

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = modles.CharField(max_length = 128, unique = True)
+    
+    def __str__ (self):
+        
+        return self.name
+    
+
+class Page(models.Model):
+    
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 128)
+    
+    url = models.URLFiels()
+    views = models.IntegerField(default = 0)
+    
+    def __str__ (self):
+        
+        return self.title
+    
+    
+
+    
+    
+        
+        
