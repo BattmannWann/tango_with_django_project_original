c68270411b234cc718e1b22deadedf7a9644d965
Rhys Stewart <2682261s@student.gla.ac.uk>
2024-01-24 13:13:41 +0000

    Fixed typo in models.py class Category

--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
-    name = modles.CharField(max_length = 128, unique = True)
+    name = models.CharField(max_length = 128, unique = True)
     
     def __str__ (self):
         
