fe91b252b095c2700c9413ad686690f51d1b9715
Rhys Stewart <2682261s@student.gla.ac.uk>
2024-01-24 13:25:19 +0000

    Fixed typo in models.py class Page

--- rango/models.py
+++ rango/models.py
@@ -13,7 +13,7 @@ class Page(models.Model):
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
     title = models.CharField(max_length = 128)
     
-    url = models.URLFiels()
+    url = models.URLFields()
     views = models.IntegerField(default = 0)
     
     def __str__ (self):
