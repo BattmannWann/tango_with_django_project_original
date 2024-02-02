35389fba6d8379dac7bc42a80d00dc7dec8c1d2a
Rhys Stewart <2682261s@student.gla.ac.uk>
2024-02-02 19:43:46 +0000

    Completed Chapter 7, reformatted a lot of the .html files to meet EXACT string testing of test files

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,8 +1,3 @@
-from rango import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 
 """tango_with_django_project URL Configuration
 
@@ -22,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from django.urls import include 
+from django.urls import include
+ 
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
