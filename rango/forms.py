0c8893f940a1e958e7e1265a5eeb58a17ba61259
Rhys Stewart <2682261s@student.gla.ac.uk>
2024-02-02 17:40:19 +0000

    Correctly implemented the add_category task of 7.2, website functional

--- rango/forms.py
+++ rango/forms.py
@@ -13,19 +13,19 @@ class CategoryForm(forms.ModelForm):
     
     class Meta:
         model = Category
-        fields = ('name')
+        fields = ('name', )
         
         
 
 class PageForm(forms.ModelForm):
     
     title = forms.CharField(max_length = 128, help_text = "Please enter the title of the page.")
-    url = forms.URLField(max_length = 200, help_text = "PLease enter the URL of the page.")
-    views = forms.IntegerField(widget = forms.HiddenInput(), intital = 0)
+    url = forms.URLField(max_length = 200, help_text = "Please enter the URL of the page.")
+    views = forms.IntegerField(widget = forms.HiddenInput(), initial = 0)
     
     class Meta:
         
         model = Page
-        exclude = ('category')
+        exclude = ('category', )
         
         
